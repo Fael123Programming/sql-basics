e17992284f9cc2a1e843921bebc0292d053e7e4e
Fael123Programming <rafaelfonseca1020@gmail.com>
2022-03-10 14:42:30 -0300

    training SQL commands

---
+++ insert_data_into_a_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO contacts (first_name, last_name, email, phone)
+VALUES ('Pedro', 'Siqueira', 'pedro.siqueira@gmail.com', NULL), 
+('Rafael', 'Guimarães', 'rafael.guimaraes@gmail.com', '+5565991374852');
---
+++ select_data_from_a_table_01.sql
@@ -0,0 +1,3 @@
+SELECT * FROM contacts WHERE phone IS NULL; /*Use IS NULL operator to check whether a value is missing or not in a row.*/
+
+SELECT * FROM contacts WHERE phone IS NOT NULL; /*The opposite of the command above.*/
